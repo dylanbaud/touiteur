5dfcef1ef93d0a343425034ff4febd39f22415ea
Okiles <colsonlenny5@gmail.com>
2023-11-06 14:45:15 +0100

    Added logo

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/logo.png">
+    <title>Touiteur | Accueil</title>
+</head>
+<body>
 
-print "Touiteur";
+</body>
+</html>
+
+
+<?php print "Touiteur"; ?>
