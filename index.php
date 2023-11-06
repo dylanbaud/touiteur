75399c1df3f95bc5b96d3a0376a80913acad1ccb
Washi <50027934+WashiFR@users.noreply.github.com>
2023-11-06 14:46:54 +0100

    Update

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" href="img/logo.png">
-    <title>Touiteur | Accueil</title>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+include 'index.html';
 
-
-<?php print "Touiteur"; ?>
+print "Touiteur";
