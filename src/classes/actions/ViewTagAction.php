eed93e70260d31049bfd3d6fb8ea28cb96715b66
Okiles <colsonlenny5@gmail.com>
2023-11-08 23:00:21 +0100

    Working tag

--- src/classes/actions/ViewTagAction.php
+++ src/classes/actions/ViewTagAction.php
@@ -12,6 +12,11 @@ class ViewTagAction extends Action
 
     public function execute(): string
     {
+
+        if(!isset($_GET['page'])){
+            $_GET['page'] = 1;
+        }
+        
         $id = $_GET['tag'];
         $render = new PostListRender(Tag::getPostListTag($id));
         return $render->render();
