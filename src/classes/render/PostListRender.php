c6c4e2b8798194b6ef1ef9038570ff1565f1cb8b
Okiles <colsonlenny5@gmail.com>
2023-11-08 15:39:57 +0100

    Update PostListRender.php

--- src/classes/render/PostListRender.php
+++ src/classes/render/PostListRender.php
@@ -77,10 +77,12 @@ HTML;
             <div class="pagination">
             HTML;
             for ($i = 1; $i <= $pageCount; $i++){
-                if($i = $_GET['page'])
-                    $html .= '<a href="?action=view-profile&page='.$i.'&id='.$_GET['id'].'" id="current-page">'.$i.'</a>';
-                else
-                    $html .= '<a href="?action=view-profile&page='.$i.'&id='.$_GET['id'].'">'.$i.'</a>';
+//                if($i = $_GET['page']){
+//                    $html .= '<a href="?action=view-profile&page='.$i.'&id='.$_GET['id'].'" id="current-page">'.$i.'</a>';
+//                } else {
+//                    $html .= '<a href="?action=view-profile&page='.$i.'&id='.$_GET['id'].'">'.$i.'</a>';
+//                }
+                $html .= '<a href="?action=view-profile&page='.$i.'&id='.$_GET['id'].'">'.$i.'</a>';
             }
         }
 
