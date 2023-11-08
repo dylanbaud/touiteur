4669d209b4611cfbff8b39ebfddcbac42458bf81
Okiles <colsonlenny5@gmail.com>
2023-11-08 15:44:07 +0100

    Update PostListRender.php

--- src/classes/render/PostListRender.php
+++ src/classes/render/PostListRender.php
@@ -58,10 +58,7 @@ HTML;
             if(!isset($_GET['page']))
                 $_GET['page'] = 1;
             for ($i = 1; $i <= $pageCount; $i++){
-                if($i = $_GET['page'])
-                    $html .= '<a href="?action=default&page='.$i.'" id="current-page">'.$i.'</a>';
-                else
-                    $html .= '<a href="?action=default&page='.$i.'">'.$i.'</a>';
+                $html .= '<a href="?action=default&page='.$i.'">'.$i.'</a>';
             }
         } else {
             $query = "select * from POST where userId = ?";
