52dd8bf5c27813f740a714430df9399b9a2f8bb4
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2023-11-08 15:05:06 +0100

    merge

--- src/classes/actions/DefaultAction.php
+++ src/classes/actions/DefaultAction.php
@@ -3,12 +3,9 @@
 namespace iutnc\touiteur\actions;
 
 use iutnc\touiteur\actions\Action;
-<<<<<<< HEAD
 use iutnc\touiteur\exception\PostException;
 use iutnc\touiteur\exception\UserException;
-=======
 use iutnc\touiteur\db\ConnectionFactory;
->>>>>>> 50eedca78275fd70a7ed712d9edb7ec7cdf09cd7
 use iutnc\touiteur\post\PostList;
 use iutnc\touiteur\render\PostListRender;
 use iutnc\touiteur\render\TagListRender;
@@ -26,13 +23,7 @@ class DefaultAction extends Action
         }
         $postList = PostList::getAllPosts($min);
         $postListRender = new PostListRender($postList);
-<<<<<<< HEAD
         $tagListRender = new TagListRender(TagList::getAllTags());
         return $postListRender->render() . $tagListRender->render();
-=======
-
-        return $postListRender->render();
-
->>>>>>> 50eedca78275fd70a7ed712d9edb7ec7cdf09cd7
     }
 }
