0285b535c364393f387606e5562ba8b56881c359
Okiles <colsonlenny5@gmail.com>
2023-11-08 16:02:58 +0100

    Unfinished delete post

---
+++ src/classes/actions/DeletePostAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace iutnc\touiteur\actions;
+
+use iutnc\touiteur\db\ConnectionFactory;
+
+class DeletePostAction extends Action
+{
+
+    public function execute(): string
+    {
+        $html = "";
+        $db = ConnectionFactory::makeConnection();
+        $query = 
+
+        return $html;
+    }
+}
