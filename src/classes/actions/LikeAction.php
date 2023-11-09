88d166603b58c80436d86e80a765ba0ba6c84633
Okiles <colsonlenny5@gmail.com>
2023-11-09 09:54:51 +0100

    Update LikeAction.php

--- src/classes/actions/LikeAction.php
+++ src/classes/actions/LikeAction.php
@@ -9,23 +9,21 @@ class LikeAction extends Action
 {
     public function execute(): string
     {
-        $html = '';
-
         if (!isset($_SESSION['user'])){
             header('Location: ?action=sign-in');
         }
-        elseif (isset($_GET['postId'])) {
+        else if (isset($_GET['id'])) {
             if(isset($_GET['like'])){
                 if($_GET['like'] == true){
                     $value = 1;
                 } else {
                     $value = -1;
                 }
-                User::like($_SESSION['user']->userId,$_GET['postId'], $value);
+                User::like($_SESSION['user']->userId,$_GET['id'], $value);
             }
 
-            header("Location: ?action=view-post&postId={$_GET['id']}");
+            header("Location: ?action=view-post&id={$_GET['id']}");
         }
-        return $html;
+        return "";
     }
 }
