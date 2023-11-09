46ff1c6e9c5101da1e996718ab3bf5f5ea1b6bdc
Washi <50027934+WashiFR@users.noreply.github.com>
2023-11-09 11:12:38 +0100

    Update PostListRender.php

--- src/classes/render/PostListRender.php
+++ src/classes/render/PostListRender.php
@@ -45,7 +45,7 @@ HTML;
             <a href="?action=">Pour vous</a>
             <a href="?action=view-following" class="current-choice">Abonnements</a>
 HTML;
-        } else {
+        } else if ($_GET['action'] != 'view-profile') {
             $html .= <<<HTML
             <a href="?action=" class="current-choice">Pour vous</a>
             <a href="?action=view-following">Abonnements</a>
