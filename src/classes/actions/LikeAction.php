2f176bd0579e51c7b60fc7949d1415a1fca9e48d
Okiles <colsonlenny5@gmail.com>
2023-11-09 09:51:02 +0100

    Update LikeAction.php

--- src/classes/actions/LikeAction.php
+++ src/classes/actions/LikeAction.php
@@ -2,24 +2,26 @@
 
 namespace iutnc\touiteur\actions;
 
+use iutnc\touiteur\post\Post;
 use iutnc\touiteur\user\User;
 
-class LikeAction
+class LikeAction extends Action
 {
     public function execute(): string
     {
         $html = '';
+
         if (!isset($_SESSION['user'])){
             header('Location: ?action=sign-in');
         }
-        elseif (isset($_GET['id'])) {
+        elseif (isset($_GET['postId'])) {
             if(isset($_GET['like'])){
-                if($_GET['like'] == 'true'){
+                if($_GET['like'] == true){
                     $value = 1;
                 } else {
                     $value = -1;
                 }
-                User::like($_SESSION['user']->userId,$_GET['id'], $value);
+                User::like($_SESSION['user']->userId,$_GET['postId'], $value);
             }
 
             header("Location: ?action=view-post&postId={$_GET['id']}");
