641d7393c6cbd1714d668676d733115d80e8de66
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2023-11-09 11:56:24 +0100

    maj

--- src/classes/render/PostListRender.php
+++ src/classes/render/PostListRender.php
@@ -35,7 +35,7 @@ HTML;
         }
 
         if (isset($_GET['action'])) {
-            if ($_GET['action'] == '' || $_GET['action'] = 'default') {
+            if ($_GET['action'] == '') {
                 $html .= <<<HTML
             <a href="?action=" class="current-choice">Pour vous</a>
             <a href="?action=view-following">Abonnements</a>
